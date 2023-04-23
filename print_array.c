3f3634604280b47e43861dd6d3ad4e72b308f7b0
solomonkassa <solomonmulu000@gmail.com>
2023-04-23 00:18:39 -0700

    swap

--- print_array.c
+++ print_array.c
@@ -1,3 +1,4 @@
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -9,15 +10,15 @@
  */
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+  size_t i;
 
-    i = 0;
-    while (array && i < size)
+  i = 0;
+  while (array && i < size)
     {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
+      if (i > 0)
+	printf(", ");
+      printf("%d", array[i]);
+      ++i;
     }
-    printf("\n");
+  printf("\n");
 }
