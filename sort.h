ac8479d0ae605f2a1c592fd0a5781591ba9fa92c
solomonkassa <solomonmulu000@gmail.com>
2023-04-23 00:09:21 -0700

    swap

--- sort.h
+++ sort.h
@@ -31,5 +31,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void swap(int *a, int *b);
 
 #endif
