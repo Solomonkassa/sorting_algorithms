713503a0c1a1a9b7f635738f26fc38a90b371acf
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-04-23 04:47:28 -0400

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,21 @@
 #include "sort.h"
 
 /**
+ * swap - Swaps the values of two integers
+ *
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
  * bubble_sort - sort list with bubble
  * @array: The array to be printed
  * @size: Number of elements in @array
