102363db2ccfbe8908a50b7ec9ddf511148cce69
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-04-23 04:58:47 -0400

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,12 +8,13 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i = 0, j = size;
-	int tmp;
+	int tmp,sorted;
 
 	if (array == NULL)
 		return;
 	for (j = size; j > 0; j--)
 	{
+		sorted = 0;
 		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
@@ -22,7 +23,12 @@ void bubble_sort(int *array, size_t size)
 				array[i] = array[i + 1];
 				array[i + 1] = tmp;
 				print_array(array, size);
+				sorted = 1;
 			}
+		}
+		if (sorted == 0)
+		{
+			break;
 		}
 	}
 }
