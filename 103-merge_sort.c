bdfc5a4262df0d97e0a53aefa3330d74548d01ce
Adika1630 <addisu05mulat@gmail.com>
2023-04-24 04:15:23 -0700

    Sorting algorithms

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,5 +1,9 @@
 #include "sort.h"
 
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back);
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
+
 /**
  * merge_sort - A function that sorts an array of integers in ascending
  * order using the merge sort algorithm
@@ -42,3 +46,38 @@ void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back)
 		merge_subarr(subarr, buff, front, mid, back);
 	}
 }
+
+/**
+ * merge_subarr - Sort a subarray
+ * @subarr: A subarray of an array of integers to sort
+ * @buff: A buffer to store the sorted subarray
+ * @front: The front index of the array
+ * @mid: The middle index of the array
+ * @back: The back index of the array
+ *
+ * Return: Nothing
+ */
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back)
+{
+	size_t i, j, k = 0;
+
+	printf("Merging...\n[left]: ");
+	print_array(subarr + front, mid - front);
+
+	printf("[right]: ");
+	print_array(subarr + mid, back - mid);
+
+	for (i = front, j = mid; i < mid && j < back; k++)
+		buff[k] = (subarr[i] < subarr[j]) ? subarr[i++] : subarr[j++];
+	for (; i < mid; i++)
+		buff[k++] = subarr[i];
+	for (; j < back; j++)
+		buff[k++] = subarr[j];
+	for (i = front, k = 0; i < back; i++)
+		subarr[i] = buff[k++];
+
+	printf("[Done]: ");
+	print_array(subarr + front, back - front);
+}
+
