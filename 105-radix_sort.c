2005564e3782e6a1b28149ba91f945a912cf2222
Adika1630 <addisu05mulat@gmail.com>
2023-04-24 07:31:42 -0700

    Sorting algorithms

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -2,6 +2,7 @@
 
 void radix_counting_sort(int *array, size_t size, int sig, int *buff);
 void radix_sort(int *array, size_t size);
+int get_max(int *array, int size);
 
 /**
  * radix_sort - A function that sorts an array of integers in ascending
@@ -13,20 +14,14 @@ void radix_sort(int *array, size_t size);
  */
 void radix_sort(int *array, size_t size)
 {
-	int max, i, sig, *buff;
+	int max, sig, *buff;
 
 	if (array == NULL || size < 2)
 		return;
 	buff = malloc(sizeof(int) * size);
 	if (buff == NULL)
 		return;
-	for (max = array[0], i = 0; i < size; i++)
-	{
-		if (max < array[i])
-		{
-			max = array[i];
-		}
-	}
+	max = get_max(array, size);
 	for (sig = 1; max / sig > 0; sig *= 10)
 	{
 		radix_counting_sort(array, size, sig, buff);
@@ -64,4 +59,23 @@ void radix_counting_sort(int *array, size_t size, int sig, int *buff)
 
 	for (i = 0; i < size; i++)
 		array[i] = buff[i];
+}
+
+/**
+ * get_max - Get the maximum value in an array of integers
+ * @array: An array of integers
+ * @size: The size of the array
+ * Return: The maximum integer in the array
+ */
+int get_max(int *array, int size)
+{
+	int max, i;
+
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
 }
