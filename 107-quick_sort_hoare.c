c7534bfe0018e2732b7f8153247a718d8f4ef03f
Adika1630 <addisu05mulat@gmail.com>
2023-04-25 13:17:36 -0700

    Sorting algorithms

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -71,3 +71,17 @@ int hoare_partition(int *array, size_t size, int left, int right)
 	return (above);
 }
 
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
