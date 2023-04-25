fcb8b16ffcd6db2354207c8dd8a31fe25d24c418
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-04-25 18:09:02 -0400

    Update 107-quick_sort_hoare.c

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,24 +1,6 @@
 #include "sort.h"
 
 /**
- * _swap - swaps two values in an array
- *
- * @array: data to sort
- * @i: first value
- * @j: second value
- *
- * Return: No Return
- */
-void _swap(int *array, int i, int j)
-{
-	int tmp;
-
-	tmp = array[i];
-	array[i] = array[j];
-	array[j] = tmp;
-}
-
-/**
  * partition - sorts a partition of data in relation to a pivot
  *
  * @array: data to sort
@@ -30,7 +12,7 @@ void _swap(int *array, int i, int j)
  */
 int partition(int *array, int min, int max, size_t size)
 {
-	int i, j, pivot = array[max];
+	int tmp, i, j, pivot = array[max];
 
 	for (i = min, j = max; 1; i++, j--)
 	{
@@ -42,7 +24,9 @@ int partition(int *array, int min, int max, size_t size)
 
 		if (i >= j)
 			return (i);
-		_swap(array, i, j);
+		   tmp = array[i];
+		   array[i] = array[j];
+		   array[j] = tmp;
 		print_array(array, size);
 	}
 }
