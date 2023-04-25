35f3d73d4e83a283757e4c6d1e64e8dced80ef0a
Adika1630 <addisu05mulat@gmail.com>
2023-04-25 11:29:33 -0700

    Sorting algorithms

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,2 +1,9 @@
 #include "sort.h"
 
+/**
+ * quick_sort_hoare -  function that sorts an array of integers in
+ * ascending order using the Quick sort algorithm
+ * @array: An array of integers
+ * @size: The size of the array
+ *
+ * Return: Nothing
