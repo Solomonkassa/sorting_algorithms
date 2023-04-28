d07e9e0adda529b0b98bb49730e994289c6d8337
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-04-28 15:42:13 -0400

    Create deck.h

---
+++ deck.h
@@ -0,0 +1,48 @@
+#ifndef _DECK_H_
+#define _DECK_H_
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * enum kind_e - kinds in the deck
+ *
+ * @SPADE: 0
+ * @HEART: 1
+ * @CLUB: 2
+ * @DIAMOND: 3
+ */
+typedef enum kind_e
+{
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+	const char *value;
+	const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
+void sort_deck(deck_node_t **deck);
+#endif
